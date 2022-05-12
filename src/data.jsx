db478beeec08f23b1ac5d65e1feb31053120c683
Juamez <juanmendezs.a@gmail.com>
2022-05-12 16:43:56 -0500

    add personal id from every object

--- src/data.jsx
+++ src/data.jsx
@@ -1,5 +1,6 @@
 export default [
   {
+    id: "0",
     title: "Mount Fuji",
     location: "Japan",
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
@@ -9,6 +10,7 @@ export default [
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
   },
   {
+    id: "1",
     title: "Sydney Opera House",
     location: "Australia",
     googleMapsUrl: "https://www.google.com/maps/place/%C3%93pera+de+S%C3%ADdney/@-33.8567799,151.213108,17z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m5!3m4!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967",
@@ -19,6 +21,7 @@ export default [
 
   },
   {
+    id: "2",
     title: "Geirangerfjord",
     location: "Norway",
     googleMapsUrl: "https://www.google.com/maps/place/Fiordo+de+Geiranger/@62.1049113,7.0051736,12z/data=!3m1!4b1!4m5!3m4!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817",
